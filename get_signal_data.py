12729f1094d074ac79bf1df7638683a0a6a124a4
sammy <dassamaara@gmail.com>
2023-07-17 15:17:12 +0530

    recieving alert data for entries
    recieving only the entries of the alert. we get this info everytime a new alert pops up

--- get_signal_data.py
+++ get_signal_data.py
@@ -3,6 +3,7 @@ This module gets the entry signals from the premium screener on tradingview
 by getting data from alerts
 '''
 
+# import modules
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -17,11 +18,13 @@ from selenium.webdriver.common.by import By
 # some constants
 CHROME_PROFILE_PATH = 'C:\\Users\\Puja\\AppData\\Local\\Google\\Chrome\\User Data'
 DRIVER_PATH = 'C:\\Users\\Puja\\chromedriver'
-EMAIL = 'dassamaara@gmail.com'
-PWD = '1304sammy#'
+# EMAIL = 'dassamaara@gmail.com'
+# PWD = '1304sammy#'
+EMAIL = 'nili.thp.work@gmail.com'
+PWD = 'Das12345'
 
 
-
+# class
 class Browser:
 
   def __init__(self, driver: str, keep_open: bool) -> None:
@@ -47,39 +50,41 @@ class Browser:
     self.open_page("https://www.tradingview.com/#signin")
 
     # click sign in (google fills our email & pwd automatically so we can just immediately click on sign in)
-    sign_in_btn = self.driver.find_element(By.CSS_SELECTOR, ".submitButton-LQwxK8Bm.button-D4RPB3ZC.size-large-D4RPB3ZC.color-brand-D4RPB3ZC.variant-primary-D4RPB3ZC.stretch-D4RPB3ZC")
-    sign_in_btn.click()
+    try:
+      sign_in_btn = self.driver.find_element(By.CSS_SELECTOR, ".submitButton-LQwxK8Bm.button-D4RPB3ZC.size-large-D4RPB3ZC.color-brand-D4RPB3ZC.variant-primary-D4RPB3ZC.stretch-D4RPB3ZC")
+      sign_in_btn.click()
+    except:
+      print('⛔ Couldn\'t sign in')
 
   def read_alert(self, msg):
-    # Split the text into lines
-    lines = msg.split('\n')  
-    print(msg)
-    self.close_alert()
-
-    # lists for sell, buy entries and closed entries
-    sell_list = []
     buy_list = []
+    sell_list = []
     closed_buy_list = []
     closed_sell_list = []
 
-    # go thru each line to see which one is about a buy or sell or a closed trade
-    for i, line in enumerate(lines):
-      # break the line up
+    lines = msg.split('\n')
+    self.close_alert()
+
+    print('📖  reading lines')
+
+    for line in lines:
       parts = line.split('|')
 
       if 'Buy' in line:
-        buy_list.append({'entry': parts[0], 'tp': parts[1], 'sl': parts[2], 'symbol': parts[3], 'timeframe': parts[4]})
-      
+        buy_list.append({'entry': parts[1], 'tp': parts[2], 'sl': parts[3], 'symbol': parts[4], 'timeframe': parts[5]})
+        print(buy_list[len(buy_list)-1])
+
       if 'Sell' in line:
-        sell_list.append({'entry': parts[0], 'tp': parts[1], 'sl': parts[2], 'symbol': parts[3], 'timeframe': parts[4]})
-      
-      # if 'Closed Buy' in line and 'TP' in line:
+        sell_list.append({'entry': parts[1], 'tp': parts[2], 'sl': parts[3], 'symbol': parts[4], 'timeframe': parts[5]})
+        print(sell_list[len(sell_list)-1])
+
+      # if 'Closed Sell' in line and 'TP' in line: 
       #   closed_sell_list.append({'entry': parts[0], 'symbol': parts[1], 'timeframe': parts[2]})
+      #   print(closed_sell_list[len(closed_sell_list)-1])
 
       # if 'Closed Buy' in line and 'TP' in line:
       #   closed_buy_list.append({'entry': parts[0], 'symbol': parts[1], 'timeframe': parts[2]})
-
-    
+      #   print(closed_buy_list[len(closed_buy_list)-1])
 
 
   def click_products_tab(self):
@@ -102,14 +107,10 @@ class Browser:
     ok_button.click()
 
   def get_data_from_alert(self):
-    # the code below only works when there is an alert setup in our account.
-    # if there is no alert set up in the alerts tab, create one for the premium screener
-    alert_msg = None
 
     while True:
       try:
         alert_msg = WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, "div.secondaryRow-QkiHQU0S")))
-        print('🔔 recieved alert')
         self.read_alert(alert_msg.text)
         alert_msg = None
       except Exception as e:
@@ -130,7 +131,10 @@ browser.get_data_from_alert()
 
 
 # 🎯 TO-DO
-# classify the buys, sells and exits into dictionaries
+# 🟢 classify the buys, sells and exits into dictionaries
 
-# after getting the text from the alert, read it. figure out if its about an entry or an exit (use regex)
-# after knowing if its an entry or exit, go to that chart & timeframe
+# 🟢 after getting the text from the alert, read it. figure out if its about an entry or an exit (use regex)
+
+# 🟡 print out the classified entries and exits in their lists and dictionaries
+
+# 🔴 after knowing if its an entry or exit, go to that chart & timeframe
