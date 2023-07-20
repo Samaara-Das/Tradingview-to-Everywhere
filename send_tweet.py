3ae5981a7c01e081e42557d89a831c4c591ac410
araamas <dassamaara@gmail.com>
2023-07-21 01:11:40 +0530

    changes for the tweepy library

--- send_tweet.py
+++ send_tweet.py
@@ -2,121 +2,18 @@
 this file is for sending snapshots from tradingview to twitter
 '''
 
-import time
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+import tweepy
+import keys
 
+def api():
+    auth = tweepy.OAuthHandler(keys.api_key, keys.api_secret)
+    auth.set_access_token(keys.access_token, keys.access_token_secret)
 
-# some constants
-CHROME_PROFILE_PATH = 'C:\\Users\\Puja\\AppData\\Local\\Google\\Chrome\\User Data'
-DRIVER_PATH = 'C:\\Users\\Puja\\chromedriver'
-EMAIL = 'dassamaara@gmail.com'
-PWD = '1304sammy#'
+def tweet(api: tweepy.API, message: str, image_path=None):
+    if image_path:
+        api.update_status_with_media(message, image_path)
+    else:
+        api.update_status(message)
 
+    print('Tweeted successfully!')
 
-
-class Browser:
-
-  def __init__(self, driver: str, keep_open: bool) -> None:
-    self.service = Service(driver)
-    chrome_options = Options()
-    chrome_options.add_experimental_option("detach", keep_open)
-
-    # make sure that any other chrome browser is closed otherwise it wont work
-    chrome_options.add_argument(f"--user-data-dir={CHROME_PROFILE_PATH}")
-    self.driver = webdriver.Chrome(service=self.service, options=chrome_options)
-
-  def open_page(self, url: str):
-    self.driver.get(url)
-    self.driver.maximize_window()
-  
-  def close_page(self):
-    time.sleep(5)
-    print("shutting down browser 💤")
-    self.driver.close()
-
-  def sign_in(self):
-    # open the sign in page
-    self.open_page("https://www.tradingview.com/#signin")
-
-    # click sign in (google fills our email & pwd automatically so we can just immediately click on sign in)
-    sign_in_btn = self.driver.find_element(By.CSS_SELECTOR, ".submitButton-LQwxK8Bm.button-D4RPB3ZC.size-large-D4RPB3ZC.color-brand-D4RPB3ZC.variant-primary-D4RPB3ZC.stretch-D4RPB3ZC")
-    sign_in_btn.click()
-
-  def close_pinescript_panel(self):
-    try:
-      minimize_panel = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'button[aria-label="Hide panel"]')))
-      minimize_panel.click()
-    except Exception as e:
-      print(f"⛔ Couldn't close panel: {str(e)}")
-
-  def click_products_tab(self):
-    while True:
-      try:
-          # Wait for the interfering element to disappear
-          WebDriverWait(self.driver, 10).until(EC.invisibility_of_element_located((By.CSS_SELECTOR, ".header-v4NPAtHi")))
-          
-          # Click on the "Products" tab
-          product_tab = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, "Products")))
-          product_tab.click()
-          
-          # break the loop if we click on the element
-          break
-      except Exception as e:
-          print(f"⛔ Error: {str(e)}")
-
-  def click_tweet_image(self):
-    # click the camera button on the top-right i.e "Take a snapshot"
-    WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button[aria-label='Take a snapshot'] > div#header-toolbar-screenshot"))).click()
-    
-    # click the "tweet image" button
-    tweet_image = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[data-name="tweet-chart-image"]')))
-    tweet_image.click()
-
-
-  def sign_in_to_twitter_and_tweet(self):
-    # Get the current window handle
-    current_window = self.driver.current_window_handle
-
-    # Wait for a new window to open
-    WebDriverWait(self.driver, 10).until(EC.new_window_is_opened(self.driver.window_handles))
-
-    # Switch to the new window
-    for window_handle in self.driver.window_handles:
-        if window_handle != current_window:
-            self.driver.switch_to.window(window_handle)
-            break
-
-    # Click the Tweet button
-    sendTw = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//div[@role="button"]/div/span/span')))
-    sendTw.click()
-
-
-  def save_chart_img(self):
-    ActionChains(self.driver).key_down(Keys.ALT).send_keys('s').perform()
-    element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.LINK_TEXT, 'Save image')))
-    element.click() 
-
-
-browser = Browser(DRIVER_PATH, True)
-
-# sign in to the browser
-browser.sign_in()
-
-# click on "Products" tab
-browser.click_products_tab()
-
-# close pinescript panel
-browser.close_pinescript_panel()
-
-# click on "Tweet Image" button
-browser.click_tweet_image()
-
-# sign in to twitter
-browser.sign_in_to_twitter_and_tweet()
