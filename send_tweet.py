9040cfd26905314c6ba353ba044d1993046089bc
sammy <dassamaara@gmail.com>
2023-07-22 20:07:14 +0530

    used the api calls to send posts to twitter

--- send_tweet.py
+++ send_tweet.py
@@ -4,12 +4,84 @@ this file is for sending snapshots from tradingview to twitter
 
 import tweepy
 from keys import *
+from requests_oauthlib import OAuth1Session
+
+consumer_key = api_key
+consumer_secret = api_secret
+
+# Be sure to add replace the text of the with the text you wish to Tweet. You can also add parameters to post polls, quote Tweets, Tweet with reply settings, and Tweet to Super Followers in addition to other features.
+payload = {"text": "hi"}
+
+
+# Get request token
+request_token_url = "https://api.twitter.com/oauth/request_token?oauth_callback=oob&x_auth_access_type=write"
+oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+try:
+    fetch_response = oauth.fetch_request_token(request_token_url)
+except ValueError:
+    print(
+        "There may have been an issue with the consumer_key or consumer_secret you entered."
+    )
+
+
+resource_owner_key = fetch_response.get("oauth_token")
+resource_owner_secret = fetch_response.get("oauth_token_secret")
+print("Got OAuth token: %s" % resource_owner_key) #OAuth token: dnuwKwAAAAABoPzPAAABiX3VFjo
+
+# Get authorization
+base_authorization_url = "https://api.twitter.com/oauth/authorize"
+authorization_url = oauth.authorization_url(base_authorization_url)
+print("Please go here and authorize: %s" % authorization_url)
+verifier = input("Paste the PIN here: ")
+
+
+# Get the access token
+access_token_url = "https://api.twitter.com/oauth/access_token"
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=resource_owner_key,
+    resource_owner_secret=resource_owner_secret,
+    verifier=verifier,
+)
+oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+access_token = oauth_tokens["oauth_token"]
+access_token_secret = oauth_tokens["oauth_token_secret"]
+
+# Make the request
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=access_token,
+    resource_owner_secret=access_token_secret,
+)
+
+# Making the request
+response = oauth.post(
+    "https://api.twitter.com/2/tweets",
+    json=payload,
+)
+
+# this happened... we got this in the console:
+# Request returned an error: 429 {"title":"Too Many Requests","detail":"Too Many Requests","type":"about:blank","status":429}
+if response.status_code != 201:
+    raise Exception(
+        "Request returned an error: {} {}".format(response.status_code, response.text)
+    )
+
+print("Response code: {}".format(response.status_code))
+
+
+
+
 
 class TwitterClient:
     def __init__(self):
         self.client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
         self.auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret) 
-        self.api = tweepy.API(self.auth)
+        self.api = tweepy.API(self.auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 
     def create_tweet(self, text):
         self.client.create_tweet(text=text)
