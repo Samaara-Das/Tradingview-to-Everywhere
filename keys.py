e9978050535bae1b117ba10917cf71a95ecaccbf
sammy <dassamaara@gmail.com>
2023-07-22 20:56:56 +0530

    added new keys & secrets for nili.thp account

--- keys.py
+++ keys.py
@@ -1,3 +1,5 @@
+
+# these are for my own account
 api_key = 'gx8Uxed9AeiU4VYYisyfmXsz8'
 api_secret = '3i3IA6R1O0HTIyNby8TvxHe4ExNTU9yTPyKUSJVkKGAtAbF0X1'
 
@@ -5,3 +7,19 @@ bearer_token = r'AAAAAAAAAAAAAAAAAAAAAM%2F8oAEAAAAALkY4x84f8qk1PMlgv5TTIjBUiYk%3
 
 access_token = '1496761536953864194-ISDErd59d4OkOlnuPCVu7akibjhFpf'
 access_token_secret = 'JBOHAIfWNyLQqhthnfIxPeWtk42dnFqohtwtTeImr0Sii'
+
+
+
+# these are for the nili.thp.work account
+api_key2 = '72G2uO5AyJfj8vhgDgnIsadip'
+api_secret2 = 'hHn82IWQLrPCcFnH6YSDCdfVh0OlpYoZ1tURpJlp0Pn8a4mql5'
+
+bearer_token2 = r'AAAAAAAAAAAAAAAAAAAAAELSowEAAAAAChjeG0wtdBUIhbLkrCPgsk3q9l0%3DC3QhfuxBS7131jpcaG5HUnezk4qyDVtQQs2oQb6Hdh1WFJWZi9'
+
+access_token2 = '1682766611533672452-4OMVioLYlQMXIfr5p3uYC69SClnWeh'
+access_token_secret2 = 'VkHrdU9FC6JheBo0LVDfKz4YiwbC1MgYVtQKiFSUVqmGe'
+
+# OAuth 2.0 Client ID and Client Secret
+client_id = 'UFBIV2RxSmhDQnhRaldRaXdmdWo6MTpjaQ'
+client_secret = 'WvF2NSIR_YtUmKUlD79OlZx8tVG2MPxfETRua0ZPturRrI95YC'
+
