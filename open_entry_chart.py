a5de1e02e9f9bf81c3f5a3f21672eb545ff5ff91
sammy <dassamaara@gmail.com>
2023-07-24 15:18:11 +0530

    seems to finally open a new tab, take its url and put it on twitter and then continues with the rest of the charts and doesn't stop midways

--- open_entry_chart.py
+++ open_entry_chart.py
@@ -50,18 +50,15 @@ class OpenChart:
     # click on submit
     self.driver.find_element(By.CSS_SELECTOR, 'button[name="submit"]').click()
 
-
   def change_symbol(self, symbol):
     # only search for a specific symbol if the current symbol is different from that symbol
     symbol_search = WebDriverWait(self.driver, 15).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="header-toolbar-symbol-search"]')))
-    if symbol_search.find_element(By.CSS_SELECTOR, 'div').text == symbol:
-      return
-    
+    if not symbol_search.find_element(By.CSS_SELECTOR, 'div').text == symbol:
     # click on Symbol Search and search for a specific symbol and hit ENTER
-    symbol_search.click()
-    search_input = self.driver.find_element(By.XPATH, '//*[@id="overlap-manager-root"]/div/div/div[2]/div/div[2]/div[1]/input')
-    search_input.send_keys(symbol)
-    search_input.send_keys(Keys.ENTER)
+      symbol_search.click()
+      search_input = self.driver.find_element(By.XPATH, '//*[@id="overlap-manager-root"]/div/div/div[2]/div/div[2]/div[1]/input')
+      search_input.send_keys(symbol)
+      search_input.send_keys(Keys.ENTER)
 
   def change_tframe(self, timeframe):
     # click on the dropdown and choose from the dropdown options and click on the one which matches the timeframe
@@ -81,20 +78,17 @@ class OpenChart:
     open_in_new_tab = self.driver.find_element(By.XPATH, '//*[@id="overlap-manager-root"]/div/span/div[1]/div/div/div[5]')
     open_in_new_tab.click()
 
-    # get the link of the newly opened tab after it has fully loaded
+    # get the url of the newly opened tab after it has fully loaded
     self.driver.switch_to.window(self.driver.window_handles[-1])
     WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'img[class="tv-snapshot-image"]')))
-    print('url of current tab: ', self.driver.current_url)
+    url = self.driver.current_url
 
-    # close the tab 
-    # self.driver.close()
+    # close the new tab 
+    self.driver.close()
 
     # switch back to the original tab
-    # self.driver.switch_to.window(self.window_handles[0])
+    self.driver.switch_to.window(self.driver.window_handles[0])
 
-    # get the saved link of the chart from the clipboard
-    link = pyperclip.paste()
-    link = pyperclip.paste()
-    return link
+    return url
   
   
