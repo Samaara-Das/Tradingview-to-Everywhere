0cd260d2d8f3fce90d35d65b9b4e04bb93d5ab85
sammy <dassamaara@gmail.com>
2023-07-31 16:41:52 +0530

    can now open 9 tabs with a low probability of the error coming on the alerts

--- send_tweet.py
+++ send_tweet.py
@@ -8,7 +8,7 @@ from keys import get_tokens_of_account
 
 class TwitterClient:
     def __init__(self):
-        bearer_token, api_key, api_secret, access_token, access_token_secret = get_tokens_of_account(1)
+        bearer_token, api_key, api_secret, access_token, access_token_secret = get_tokens_of_account(2)
         self.client = tweepy.Client(bearer_token, api_key, api_secret, access_token, access_token_secret)
         self.auth = tweepy.OAuth1UserHandler(api_key, api_secret, access_token, access_token_secret) 
         self.api = tweepy.API(self.auth, wait_on_rate_limit=True)
