29f27b936f162e59589f1d39767dbc947095d825
sammy <dassamaara@gmail.com>
2023-08-02 22:23:16 +0530

    connected to remote database

--- database.py
+++ database.py
@@ -1,13 +1,16 @@
 import pymongo
 from pymongo.mongo_client import MongoClient
 
-PWD = '1304sammy#'
+PWD = '5gsCKHt4Dg4aSa8E'
 
 class Database:
     def __init__(self):
-        self.pwd = PWD
-        # the connection string was from the mongo shell when i typed "mongosh"
-        self.client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.3")
+        self.cluster_pwd = PWD
+        # for a connection to local database (the connection string was from the mongo shell when i typed "mongosh"):
+        # self.client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.3")
+
+        # for a connection to remote database:
+        self.client = MongoClient(f"mongodb+srv://samaara:{self.cluster_pwd}@cluster1.565lfln.mongodb.net/?retryWrites=true&w=majority")
         
         # Send a ping to confirm a successful connection
         try:
@@ -19,7 +22,7 @@ class Database:
         self.db = self.client["test"]
         self.collection = self.db["Entries & Exits"]
 
-    def add_doc(self, _type, direction, symbol, tframe, entry, tp, sl, chart_link, date):
+    def add_doc(self, _type, direction, symbol, tframe, entry, tp, sl, chart_link, content, date):
         doc = {
             "type": _type,
             "direction": direction,
@@ -29,6 +32,7 @@ class Database:
             "tp": tp,
             "sl": sl,
             "chart_link": chart_link,
+            "content": content,
             "date": date
         }
         return self.collection.insert_one(doc)
@@ -40,3 +44,5 @@ class Database:
     def delete_all(self):
         self.collection.delete_many({}) 
 
+db = Database()
+db.delete_all()
