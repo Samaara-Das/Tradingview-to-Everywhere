485bed0b8473e09b7ecbb48b5366c7b27104a77a
sammy <dassamaara@gmail.com>
2023-08-04 14:50:38 +0530

    successfully sending data to nk uncle's webhook link

--- send_to_db.py
+++ send_to_db.py
@@ -2,11 +2,23 @@
 this is how we send post requests to Nishant uncle's database
 '''
 
-from requests import post
+from requests import Session
+from requests.adapters import HTTPAdapter
+from requests.exceptions import ConnectionError
 
 class Post:
-  def __init__(self):
+  def __init__(self, max_retries=3):
     self.url = 'https://pointcapitalis.com/meta/addTradeViewData'
+    self.adapter = HTTPAdapter(max_retries=max_retries)
+    self.session = Session()
+    self.session.mount(self.url, self.adapter)
 
   def post_to_url(self, payload: dict):
-    post(self.url, data=payload)
+    try:
+      response = self.session.post(self.url, data=payload)
+    except ConnectionError as ce:
+      print(ce)
+    else:
+      return response
+    
+
