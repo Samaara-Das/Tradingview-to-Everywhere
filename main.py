0836976dfc6415f8b24a5c380d9b3b970d2bae89
sammy <dassamaara@gmail.com>
2024-02-24 21:08:09 +0530

    this code has the clear_cache function

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import logger_setup
 import open_tv
 from time import time
 from time import sleep
+from selenium.webdriver.support.ui import Select
 
 # Set up logger for this file
 main_logger = logger_setup.setup_logger(__name__, logger_setup.logging.DEBUG)
@@ -25,6 +26,31 @@ if REMOVE_LOG:
     with open('app_log.log', 'w') as file:
         pass
 
+def clear_cache(driver):
+    '''this clears the cache of the browser in the last hour'''
+    try:
+        # open the clear browser cache page
+        driver.execute_script('window.open("about:blank", "_blank");') 
+        driver.switch_to.window(driver.window_handles[1])
+        driver.get('chrome://settings/clearBrowserData') 
+        sleep(2) # wait for the page to load
+
+        # Select the "Last hour" option from the dropdown
+        time_range_dropdown = driver.find_element(open_tv.By.CSS_SELECTOR, 'select[aria-label="Time range"]')
+        time_range_dropdown.select_by_visible_text('Last hour')
+        main_logger.info('Selected "Last hour" as time range')
+~
+        # Click the "Browsing history" and "Cached images and files"
+        driver.find_element(open_tv.By.CSS_SELECTOR, 'settings-checkbox[id="browsingCheckboxBasic"] div[id="checkbox"]').click()
+        driver.find_element(open_tv.By.CSS_SELECTOR, 'settings-checkbox[id="cacheCheckboxBasic"] div[id="checkbox"]').click()
+        main_logger.info('Clicked on the "Browsing history" and "Cached images and files" checkboxes')
+
+        # Click the "Clear data" button
+        driver.find_element(open_tv.By.CSS_SELECTOR, 'div[slot="button-container"] cr-button[id="clearBrowsingDataConfirm"]').click()
+        main_logger.info('Clicked on "Clear data"')
+    except Exception as e:
+        main_logger.exception(f'Error occurred when clearing cache:')
+
 # Run main code
 if __name__ == '__main__':
     try:
@@ -33,6 +59,8 @@ if __name__ == '__main__':
 
         # initiate Browser
         browser = open_tv.Browser(True, SCREENER_SHORT, SCREENER_NAME, DRAWER_SHORT, DRAWER_NAME, INTERVAL_MINUTES)
+
+        clear_cache(browser.driver)
 
         # setup the indicators, alerts etc.
         setup_check = browser.setup_tv()
@@ -45,13 +73,7 @@ if __name__ == '__main__':
             while True:
                 # restart all the inactive alerts every INTERVAL_MINUTES minutes (this is also done in get_alert_data.py in the method get_alert_box_and_msg()) and refresh browser
                 if time() - last_run > interval_seconds:
-                    try:
-                        browser.driver.refresh()
-                        browser.WebDriverWait(browser.driver, 10).until(lambda x: x.execute_script('return document.readyState') == 'complete') # wait for the page to re-load after refreshing it
-                        main_logger.info('Browser refreshed to prevent browser from getting frozen.')
-                    except:
-                        main_logger.exception('Failed to refresh browser. Error:')
-                    
+                    clear_cache()
                     browser.alerts.restart_inactive_alerts()
                     last_run = time()
 
