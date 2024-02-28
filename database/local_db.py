335182114d983d33da235da2f9e2893314571f0b
sammy <dassamaara@gmail.com>
2024-02-28 11:20:02 +0530

    latest commit

--- database/local_db.py
+++ database/local_db.py
@@ -14,7 +14,7 @@ local_db_logger = logger_setup.setup_logger(__name__, logger_setup.logging.DEBUG
 PWD = 'kdgzKyjYr8WA6Vkm'
 
 class Database:
-    def __init__(self, delete=False):
+    def __init__(self, col, delete=False):
         # for a connection to local database (the connection string was from the mongo shell when i typed "mongosh"):
         # self.client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.3")
 
@@ -29,33 +29,33 @@ class Database:
             return
         
         self.db = self.client["tradingview-to-everywhere"]
-        self.collection = self.db["entries"]
 
         if delete:
-            self.delete_all()
+            self.delete_all(col)
             local_db_logger.info("Successfully deleted all documents")
 
-    def add_doc(self, doc: dict):
+    def add_doc(self, doc: dict, col: str):
+        '''Adds `doc` to a specific collection'''
         try:
-            self.collection.insert_one(doc)
-            local_db_logger.info(f"Successfully sent a doc to MongoDb!")
+            self.db[col].insert_one(doc)
+            local_db_logger.info(f"Successfully sent a doc to {col} collection!")
             return True
         except Exception as e:
-            local_db_logger.exception(f'Failed to add document to our local database\'s collection. Error:')
+            local_db_logger.exception(f'Failed to add document to MongoDB\'s {col} collection. Error:')
             return False
 
-    def get_latest_doc(self):
-        docs = self.collection.find_one(sort=[("_id", pymongo.DESCENDING)])
+    def get_latest_doc(self, col: str):
+        docs = self.db[col].find_one(sort=[("_id", pymongo.DESCENDING)])
         return docs
 
-    def delete_all(self):
-        self.collection.delete_many({}) 
+    def delete_all(self, col: str):
+        self.db[col].delete_many({}) 
 
-    def delete_some(self, count: int):
+    def delete_some(self, count: int, col: str):
         '''This will keep the latest `count` documents and deletes the rest of them. It will also log how many docs got deleted'''
         
-        latest_100_ids = [x['_id'] for x in self.collection.find().sort('_id', pymongo.DESCENDING).limit(count)] # Get the IDs of the latest 100 documents
-        result = self.collection.delete_many({'_id': {'$nin': latest_100_ids}}) # Delete all documents that are not in the latest 100
-        local_db_logger.info(f"Deleted {result.deleted_count} documents.")
+        latest_100_ids = [x['_id'] for x in self.db[col].find().sort('_id', pymongo.DESCENDING).limit(count)] # Get the IDs of the latest 100 documents
+        result = self.db[col].delete_many({'_id': {'$nin': latest_100_ids}}) # Delete all documents that are not in the latest 100
+        local_db_logger.info(f"Deleted {result.deleted_count} documents from {col} collection.")
 
 
--- main.py
+++ main.py
@@ -16,7 +16,7 @@ DRAWER_SHORT = 'Trade' # short title of the trade drawer indicator
 SCREENER_NAME = 'Premium Screener' # name of the screener
 DRAWER_NAME = 'Trade Drawer' # name of the trade drawer
 REMOVE_LOG = True # remove the content of the log file (to clean it up)
-INTERVAL_MINUTES = 60 # number of mins to wait until inactive alerts get reactivated and for the browser to refresh (refreshing will hopefully prevent the browser and this application from freezing)
+INTERVAL_MINUTES = 5 # number of mins to wait until inactive alerts get reactivated and for the browser to refresh (refreshing will hopefully prevent the browser and this application from freezing)
 
 # Convert the interval to seconds
 interval_seconds = INTERVAL_MINUTES * 60
@@ -26,30 +26,30 @@ if REMOVE_LOG:
     with open('app_log.log', 'w') as file:
         pass
 
-def clear_cache(driver):
-    '''this clears the cache of the browser in the last hour'''
-    try:
-        # open the clear browser cache page
-        driver.execute_script('window.open("about:blank", "_blank");') 
-        driver.switch_to.window(driver.window_handles[1])
-        driver.get('chrome://settings/clearBrowserData') 
-        sleep(2) # wait for the page to load
+# def clear_cache(driver):
+#     '''this clears the cache of the browser in the last hour'''
+#     try:
+#         # open the clear browser cache page
+#         driver.execute_script('window.open("about:blank", "_blank");') 
+#         driver.switch_to.window(driver.window_handles[1])
+#         driver.get('chrome://settings/clearBrowserData') 
+#         sleep(2) # wait for the page to load
 
-        # Select the "Last hour" option from the dropdown
-        time_range_dropdown = driver.find_element(open_tv.By.CSS_SELECTOR, 'select[aria-label="Time range"]')
-        time_range_dropdown.select_by_visible_text('Last hour')
-        main_logger.info('Selected "Last hour" as time range')
-~
-        # Click the "Browsing history" and "Cached images and files"
-        driver.find_element(open_tv.By.CSS_SELECTOR, 'settings-checkbox[id="browsingCheckboxBasic"] div[id="checkbox"]').click()
-        driver.find_element(open_tv.By.CSS_SELECTOR, 'settings-checkbox[id="cacheCheckboxBasic"] div[id="checkbox"]').click()
-        main_logger.info('Clicked on the "Browsing history" and "Cached images and files" checkboxes')
+#         # Select the "Last hour" option from the dropdown
+#         time_range_dropdown = driver.find_element(open_tv.By.CSS_SELECTOR, 'select[aria-label="Time range"]')
+#         time_range_dropdown.select_by_visible_text('Last hour')
+#         main_logger.info('Selected "Last hour" as time range')
+# ~
+#         # Click the "Browsing history" and "Cached images and files"
+#         driver.find_element(open_tv.By.CSS_SELECTOR, 'settings-checkbox[id="browsingCheckboxBasic"] div[id="checkbox"]').click()
+#         driver.find_element(open_tv.By.CSS_SELECTOR, 'settings-checkbox[id="cacheCheckboxBasic"] div[id="checkbox"]').click()
+#         main_logger.info('Clicked on the "Browsing history" and "Cached images and files" checkboxes')
 
-        # Click the "Clear data" button
-        driver.find_element(open_tv.By.CSS_SELECTOR, 'div[slot="button-container"] cr-button[id="clearBrowsingDataConfirm"]').click()
-        main_logger.info('Clicked on "Clear data"')
-    except Exception as e:
-        main_logger.exception(f'Error occurred when clearing cache:')
+#         # Click the "Clear data" button
+#         driver.find_element(open_tv.By.CSS_SELECTOR, 'div[slot="button-container"] cr-button[id="clearBrowsingDataConfirm"]').click()
+#         main_logger.info('Clicked on "Clear data"')
+#     except Exception as e:
+#         main_logger.exception(f'Error occurred when clearing cache:')
 
 # Run main code
 if __name__ == '__main__':
@@ -59,8 +59,6 @@ if __name__ == '__main__':
 
         # initiate Browser
         browser = open_tv.Browser(True, SCREENER_SHORT, SCREENER_NAME, DRAWER_SHORT, DRAWER_NAME, INTERVAL_MINUTES)
-
-        clear_cache(browser.driver)
 
         # setup the indicators, alerts etc.
         setup_check = browser.setup_tv()
@@ -73,7 +71,6 @@ if __name__ == '__main__':
             while True:
                 # restart all the inactive alerts every INTERVAL_MINUTES minutes (this is also done in get_alert_data.py in the method get_alert_box_and_msg()) and refresh browser
                 if time() - last_run > interval_seconds:
-                    clear_cache()
                     browser.alerts.restart_inactive_alerts()
                     last_run = time()
 
